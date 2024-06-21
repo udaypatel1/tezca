48a7b1301f598511a1d5217a74c82fbb9b2ff325
Uday Patel <uday3patel@gmail.com>
2024-06-21 12:04:31 -0400

    feat(rayon): way faster with par_pixel iterator, need to do the same with covariance

--- src/service.rs
+++ src/service.rs
@@ -1,4 +1,4 @@
-use image::{imageops::FilterType, DynamicImage, GenericImageView, Pixel};
+use image::{imageops::FilterType, DynamicImage, GenericImageView, GrayImage, Pixel};
 use rayon::iter::{ParallelBridge, ParallelIterator};
 use std::{path::Path, sync::mpsc, thread};
 
@@ -7,62 +7,52 @@ pub fn load_image<P: AsRef<Path>>(path: P) -> DynamicImage {
 	image::open(path).expect("Failed to load image")
 }
 
-fn match_dimensions(image1: &DynamicImage, image2: &DynamicImage) -> (DynamicImage, DynamicImage) {
+fn match_dimensions(image1: &DynamicImage, image2: &DynamicImage) -> (GrayImage, GrayImage) {
 
 
 	let (width1, height1) = image1.dimensions();
 	let (width2, height2) = image2.dimensions();
 
 	if width1 == width2 && height1 == height2 {
-		(image1.clone(), image2.clone())
+		(image1.to_luma8().clone(), image2.to_luma8().clone())
 	}
 	else if (width1 * height1) > (width2 * height2) {
 
 		let resized_image_1 = image1.resize(width2, height2, FilterType::Nearest);
-		(resized_image_1, image2.clone())
+		(resized_image_1.to_luma8(), image2.to_luma8().clone())
 	}
 	else {
 		let resized_image_2 = image2.resize(width1, height1, FilterType::Nearest);
-		(image1.clone(), resized_image_2)
+		(image1.to_luma8().clone(), resized_image_2.to_luma8())
 	}
 }
 
-fn mean(image: &DynamicImage) -> f64 {
+fn mean(image: &GrayImage) -> f64 {
 
 	let (width, height) = image.dimensions();
 	let pixel_count = (width * height) as f64;
 
-	let sum: f64 = image.pixels()
-						.par_bridge()
-						.map(|pixel| {
-
-							pixel.2.to_luma().0[0] as f64
-
-						})
-						.sum();
+	let sum: f64 = image.par_pixels().map(|pixel| {
+		pixel.0[0] as f64
+	}).sum();
 
 	sum / pixel_count
 }
 
-fn variance(image: &DynamicImage, mean: f64) -> f64 {
+fn variance(image: &GrayImage, mean: f64) -> f64 {
 
 	let (width, height) = image.dimensions();
 	let pixel_count = (width * height) as f64;
 
-	let sum: f64 = image.pixels()
-						.par_bridge()
-						.map(|pixel| {
-
-							let value = pixel.2.to_luma().0[0] as f64;
-							(value - mean).powi(2)
-							
-						})
-						.sum();
+	let sum: f64 = image.par_pixels().map(|pixel| {
+		let value = pixel.0[0] as f64;
+		(value - mean).powi(2)
+	}).sum();
 
 	sum / pixel_count
 }
 
-fn covariance(image1: &DynamicImage, mean1: f64, image2: &DynamicImage, mean2: f64) -> f64 {
+fn covariance(image1: &GrayImage, mean1: f64, image2: &GrayImage, mean2: f64) -> f64 {
 
 	let (width, height) = image1.dimensions();
 	let pixel_count = (width * height) as f64;
@@ -73,8 +63,8 @@ fn covariance(image1: &DynamicImage, mean1: f64, image2: &DynamicImage, mean2: f
 					.par_bridge()
 					.map(|(p1, p2)| {
 
-						let value1 = p1.2.to_luma().0[0] as f64;
-						let value2: f64 = p2.2.to_luma().0[0] as f64;
+						let value1 = p1.0[0] as f64;
+						let value2: f64 = p2.0[0] as f64;
 
 						(value1 - mean1) * (value2 - mean2)
 					})
