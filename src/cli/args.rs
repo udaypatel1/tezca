05a90e856c1b0fd3f0d54b56b1bf97957ebc139f
Uday Patel <uday3patel@gmail.com>
2024-06-27 15:33:16 -0400

    feat(CLI): Trying a new subcommand structure WIP

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -1,13 +1,42 @@
 
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(name = "tez")]
 #[command(version = "0.1.0")]
 #[command(about = "Yet another image comparison CLI utility", long_about = None)]
+#[command(author = "Uday Patel")]
 pub struct Cli {
-	#[arg(short, long, default_value_t = 75.0, help = "Threshold for image similarity")]
-	pub threshold: f64,
+
+	#[clap(subcommand)]
+	pub subcommand: Option<Commands>,
+
+}
+
+#[derive(Subcommand)]
+#[command(about = "Subcommand Options", long_about = None)]
+enum Commands {
+
+	List(ListArgs),
+	Algorithm(Algorithm),
+}
+
+#[derive(Parser)]
+struct Algorithm {
+	#[clap(subcommand)]
+	algorithm: AlgorithmType,
+}
+
+#[derive(Subcommand)]
+enum AlgorithmType {
+	Ssim(SsimArgs),
+}
+
+#[derive(Args)]
+struct ListArgs {}
+
+#[derive(Args)]
+struct SsimArgs {
 
 	#[arg(help = "First image file to compare")]
 	pub file1: String,
--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,12 @@ mod cli;
 mod algorithms;
 
 use std::time::Instant;
-use cli::args::get_args;
+use cli::args::{get_args, Cli};
 use algorithms::ssim::{compute_ssim, load_image};
 
 fn main() {
     
     let args = get_args();
 
-    let now = Instant::now();
-
-    let img1 = load_image(&args.file1);
-    let img2 = load_image(&args.file2);
-    let threshold = args.threshold;
-
-    let elapsed = now.elapsed();
-    println!("Both file load time elapsed: {:?}", elapsed);
-
-    let now_process = Instant::now();
-
-    let ssim = compute_ssim(&img1, &img2);
-
-    let process_elapsed = now_process.elapsed();
-
-    println!("Algorithm time: {:?}", process_elapsed);
-    println!("Similarity Index: {:?}", ssim);
-    println!("Above Threshold [{:1}%]: {:?}", threshold, if (ssim * 100.) >= threshold { true } else { false });
+    
 }
